b5593a1d6463cfaed6a418a2b4bbd903b33417c5
yarsoniy <yarsoniy@gmail.com>
2023-04-15 11:15:05 +0300

    Unit tests for BillItem, Money

---
+++ tests/unit/Domain/Bill/BillItemTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Tests\unit\Domain\Bill;
+
+use App\Domain\Bill\Model\BillItem;
+use App\Domain\Money\Money;
+use App\Domain\Participant\ParticipantId;
+use Codeception\Test\Unit;
+
+class BillItemTest extends Unit
+{
+    public function testCalculatePayerSharesEqual()
+    {
+        $billItem = new BillItem('Pizza');
+        $billItem->setCost(new Money(400));
+
+        $pA = new ParticipantId('participant-A');
+        $pB = new ParticipantId('participant-B');
+        $pC = new ParticipantId('participant-C');
+        $pD = new ParticipantId('participant-D');
+        $billItem->addPaymentDirection($pA, $pA);
+        $billItem->addPaymentDirection($pB, $pB);
+        $billItem->addPaymentDirection($pC, $pC);
+        $billItem->addPaymentDirection($pD, $pD);
+
+
+        $expected = [
+            'participant-A' => new Money(100),
+            'participant-B' => new Money(100),
+            'participant-C' => new Money(100),
+            'participant-D' => new Money(100),
+        ];
+        $actual = $billItem->calculatePayerShares();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testCalculatePayerSharesAllPayForD()
+    {
+        $billItem = new BillItem('Pizza');
+        $billItem->setCost(new Money(300));
+
+        $pA = new ParticipantId('participant-A');
+        $pB = new ParticipantId('participant-B');
+        $pC = new ParticipantId('participant-C');
+        $pD = new ParticipantId('participant-D');
+
+        $billItem->addPaymentDirection($pA, $pA);
+        $billItem->addPaymentDirection($pB, $pB);
+        $billItem->addPaymentDirection($pC, $pC);
+        $billItem->addPaymentDirection($pA, $pD);
+        $billItem->addPaymentDirection($pB, $pD);
+        $billItem->addPaymentDirection($pC, $pD);
+
+
+        $expected = [
+            'participant-A' => new Money(100),
+            'participant-B' => new Money(100),
+            'participant-C' => new Money(100),
+        ];
+        $actual = $billItem->calculatePayerShares();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testCalculatePayerSharesADoesntBuyAllPayForD()
+    {
+        $billItem = new BillItem('Pizza');
+        $billItem->setCost(new Money(300));
+
+        $pA = new ParticipantId('participant-A');
+        $pB = new ParticipantId('participant-B');
+        $pC = new ParticipantId('participant-C');
+        $pD = new ParticipantId('participant-D');
+
+        $billItem->addPaymentDirection($pB, $pB);
+        $billItem->addPaymentDirection($pC, $pC);
+        $billItem->addPaymentDirection($pA, $pD);
+        $billItem->addPaymentDirection($pB, $pD);
+        $billItem->addPaymentDirection($pC, $pD);
+
+
+        $expected = [
+            'participant-A' => new Money(33.33),
+            'participant-B' => new Money(133.33),
+            'participant-C' => new Money(133.33),
+        ];
+        $actual = $billItem->calculatePayerShares();
+
+        $this->assertEqualsWithDelta($expected, $actual, 0.01);
+    }
+
+    public function testCalculatePayerSharesCPayForD()
+    {
+        $billItem = new BillItem('Pizza');
+        $billItem->setCost(new Money(400));
+
+        $pA = new ParticipantId('participant-A');
+        $pB = new ParticipantId('participant-B');
+        $pC = new ParticipantId('participant-C');
+        $pD = new ParticipantId('participant-D');
+
+        $billItem->addPaymentDirection($pA, $pA);
+        $billItem->addPaymentDirection($pB, $pB);
+        $billItem->addPaymentDirection($pC, $pC);
+        $billItem->addPaymentDirection($pC, $pD);
+
+        $expected = [
+            'participant-A' => new Money(100),
+            'participant-B' => new Money(100),
+            'participant-C' => new Money(200),
+        ];
+        $actual = $billItem->calculatePayerShares();
+
+        $this->assertEqualsWithDelta($expected, $actual, 0.01);
+    }
+}
---
+++ tests/unit/Domain/Money/MoneyTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Tests\unit\Domain\Money;
+
+use App\Domain\Money\Money;
+use Codeception\Test\Unit;
+
+class MoneyTest extends Unit
+{
+    public function testAdd()
+    {
+        $a = new Money(777.23);
+        $b = new Money(333.44);
+
+        $expected = new Money(1110.67);
+        $actual = $a->add($b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testSub()
+    {
+        $a = new Money(1110.67);
+        $b = new Money(333.44);
+
+        $expected = new Money(777.23);
+        $actual = $a->sub($b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testSplitBy3()
+    {
+        $a = new Money(1000);
+
+        $expected = [
+            new Money(333.33333),
+            new Money(333.33333),
+            new Money(333.33333),
+        ];
+        $actual = $a->split(3);
+
+        $this->assertEqualsWithDelta($expected, $actual, 0.00001);
+    }
+
+    public function testSplitBy0()
+    {
+        $a = new Money(1000);
+        $this->expectException(\DivisionByZeroError::class);
+        $a->split(0);
+    }
+
+}
