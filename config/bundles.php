e95e94df8cb9cac5faba16afc6d204c64de5f0fd
yarsoniy <yarsoniy@gmail.com>
2023-05-01 15:30:38 +0300

    install fos rest bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
