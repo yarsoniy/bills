dd94b4067e556b6efa24c10a6eb0fff6036b4d91
yarsoniy <yarsoniy@gmail.com>
2023-05-02 20:45:10 +0300

    jms serializer

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
