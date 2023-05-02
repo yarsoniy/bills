b791cb38ac1c7e7b77514995a550558aa7f998d4
yarsoniy <yarsoniy@gmail.com>
2023-05-02 20:38:16 +0300

    controllers

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
